f6de914aab761d2d1a16006a521b773dfe7fd0ac
Md Shohanur Rahman <utsho2848@gmail.com>
2022-09-27 18:36:56 +0600

    Local Storage all issu fixed and enable easier for users

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import './Cart.css'
 const Cart = ({ cart }) => {
-    const { price, shipping } = cart;
     console.log(cart)
+    // const { price, shipping } = cart;
+    let price = 0;
+    let shipping = 0;
+    let tax = 0;
+    let total = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        quantity = quantity + product.quantity;
+        price = price + product.price * product.quantity;
+        shipping = shipping + product.shipping * product.quantity;
+        tax = parseFloat((price * 0.1).toFixed(2));
+        total = price + shipping + tax;
+    }
+    // console.log(cart)
     return (
         <div className='cart'>
             <h2>This is From Cart</h2>
-            <h3>Items Selected:{cart.length}</h3>
-            <h4>Price:{ }</h4>
-            <p>Shipping Charge:{ }</p>
-            <p>Tax:{ }</p>
-            <h3>Total Cost:{ }</h3>
+            <h3>Items Selected:{quantity}</h3>
+            <h4>Price:{price}</h4>
+            <p>Shipping Charge:{shipping}</p>
+            <p>Tax:{tax}</p>
+            <h3>Total Cost:{total}</h3>
         </div>
     );
 };
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,19 +1,51 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getDataFromDb } from "../../utilities/fakedb";
 import Cart from '../Cart/Cart';
 import Products from '../Products/Products';
 import './Main.css'
 const Main = () => {
+    const [cart, setCart] = useState([]);
     const [items, setItems] = useState([])
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setItems(data))
     }, [])
-    const [cart, setCart] = useState([]);
-    const handleAddToCart = (product) => {
-        console.log(product)
-        const newCart = [...cart, product];
+    useEffect(() => {
+        const storedData = getDataFromDb();
+        const savedCart = [];
+        for (const id in storedData) {
+            const addedProduct = items.find(product => product.id === id);
+            console.log(addedProduct)
+            if (addedProduct) {
+                const quantity = storedData[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct)
+            }
+            // else {
+
+            // }
+
+        }
+        setCart(savedCart);
+    }, [items])
+
+    const handleAddToCart = (selectedProduct) => {
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id);
+
+        if (!exists) {
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else {
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists]
+        }
         setCart(newCart);
+        addToDb(selectedProduct.id)
+        // 49.5
     }
     return (
         <div className='main'>
